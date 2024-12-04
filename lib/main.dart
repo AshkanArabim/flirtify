30ff230926e93073b038b8dc1409e8c76b2513b5
AshkanArabim <ashkan.arabim@gmail.com>
2024-12-04 02:46:38 -0700

    change main to use firebase emulator in debug mode

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
-import 'package:flirtify/pages/chats_page/Chats.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+import 'package:flirtify/pages/Home.dart';
 import 'firebase_options.dart';
 
 void main() async {
@@ -12,6 +16,20 @@ void main() async {
     // uncomment line below to use the production project
     // options: DefaultFirebaseOptions.currentPlatform,
   );
+
+  if (kDebugMode) {
+    // only activeates in debug mode
+    try {
+      // firestore emulator
+      FirebaseFirestore.instance.useFirestoreEmulator('localhost', 8080);
+      // auth emulator
+      await FirebaseAuth.instance.useAuthEmulator('localhost', 9099);
+    } catch (e) {
+      // ignore: avoid_print
+      print(e);
+    }
+  }
+
   runApp(const MyApp());
 }
 
@@ -21,7 +39,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Chats(),
+      home: Home(),
     );
   }
 }
