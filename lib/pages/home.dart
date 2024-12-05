fd4b5b566f8144c92974b025130fb20b27ed0c01
AshkanArabim <ashkan.arabim@gmail.com>
2024-12-04 19:35:33 -0700

    add logout button to home sidebar

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import '../components/chats_row.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -9,9 +10,27 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  void handleLogout() async {
+    FirebaseAuth.instance.signOut();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: Drawer(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 30,
+            ),
+            DrawerRow(
+              icon: Icon(Icons.logout),
+              label: "Log Out",
+              func: handleLogout,
+            )
+          ],
+        ),
+      ),
       appBar: AppBar(
         title: Text('Clearchat'),
         backgroundColor: Colors.black12,
@@ -22,6 +41,35 @@ class _HomeState extends State<Home> {
           ChatsRow(),
           ChatsRow(),
         ],
+      ),
+    );
+  }
+
+  InkWell DrawerRow({
+    required Icon icon,
+    required GestureTapCallback func,
+    required String label,
+  }) {
+    return InkWell(
+      onTap: func,
+      child: Padding(
+        padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+        child: Row(
+          children: [
+            icon,
+            SizedBox(
+              width: 20,
+            ),
+            Expanded(
+              child: Text(
+                label,
+                style: TextStyle(
+                  fontSize: 15,
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
