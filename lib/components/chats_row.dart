f237cb2edc3873d405030eff863bfda7b6bd54b0
AshkanArabim <ashkan.arabim@gmail.com>
2024-12-06 23:39:07 -0700

    add dm avatar support to home

--- lib/components/chats_row.dart
+++ lib/components/chats_row.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class ChatsRow extends StatelessWidget {
@@ -20,7 +21,6 @@ class ChatsRow extends StatelessWidget {
 
     return InkWell(
       onTap: () {
-        print('clicked');
       },
       child: Column(
         children: [
@@ -35,21 +35,7 @@ class ChatsRow extends StatelessWidget {
               child: Row(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  CircleAvatar(
-                    child: Image.network(
-                      chat['grouppic'] ?? "",
-                      loadingBuilder: (context, child, loadingProgress) {
-                        if (loadingProgress == null) {
-                          return child;
-                        }
-
-                        return const CircularProgressIndicator();
-                      },
-                      errorBuilder: (context, error, stackTrace) {
-                        return const Icon(Icons.group);
-                      },
-                    ),
-                  ),
+                  chatAvatar(chat),
                   SizedBox(
                     width: 10,
                   ),
@@ -124,6 +110,73 @@ class ChatsRow extends StatelessWidget {
           ),
         ],
       ),
+    );
+  }
+
+  CircleAvatar chatAvatar(Map<String, dynamic> chat) {
+    Widget avatarContent;
+    if (chat['participants']!.length > 2) {
+      avatarContent = Image.network(
+        chat['grouppic'] ?? "",
+        loadingBuilder: (context, child, loadingProgress) {
+          if (loadingProgress == null) {
+            return child;
+          }
+
+          return const CircularProgressIndicator();
+        },
+        errorBuilder: (context, error, stackTrace) {
+          return const Icon(Icons.group);
+        },
+      );
+    } else {
+      Future<String> dmProfileFuture() async {
+        // get both participants of the chat
+        final participants = (chat['participants'] as List<dynamic>).map(
+          (p) => (p as DocumentReference),
+        );
+
+        // get the current user
+        final currentUserSnapshot = await FirebaseFirestore.instance
+            .collection('users')
+            .where('email', isEqualTo: FirebaseAuth.instance.currentUser!.email)
+            .get();
+        final DocumentReference currentUserRef =
+            currentUserSnapshot.docs.first.reference;
+
+        // exclude the current user from participants
+        final partnerRef = participants
+            .firstWhere((participant) => participant != currentUserRef);
+
+        // return other participant's profile link
+        final partnerSnapshot = await partnerRef.get();
+        return partnerSnapshot['profilePicURL'];
+      }
+
+      avatarContent = FutureBuilder(
+        future: dmProfileFuture(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const CircularProgressIndicator();
+          } else if (snapshot.hasError) {
+            return Icon(Icons.error);
+          } else {
+            return Image.network(
+              snapshot.data ?? "",
+              loadingBuilder: (context, child, loadingProgress) {
+                if (loadingProgress == null) {
+                  return child;
+                }
+                return CircularProgressIndicator();
+              },
+              errorBuilder: (context, error, stackTrace) => Icon(Icons.person),
+            );
+          }
+        },
+      );
+    }
+    return CircleAvatar(
+      child: avatarContent,
     );
   }
 }
